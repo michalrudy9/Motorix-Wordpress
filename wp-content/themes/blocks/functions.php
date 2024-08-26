15d782f0c6f6102fd4890956e09b249617105ae2
Michał Rudy <michallrudy9@gmail.com>
2024-08-26 19:32:01 +0200

    Hide admin bar when is user

--- wp-content/themes/blocks/functions.php
+++ wp-content/themes/blocks/functions.php
@@ -10,5 +10,15 @@ function attach_files(): void
     wp_enqueue_style('ourmaincss', get_theme_file_uri('/build/index.css'));
 }
 
+function no_admin_bar(): void
+{
+    $current_user = wp_get_current_user();
+
+    if (count($current_user->roles) == 1 and $current_user->roles[0] == 'subscriber') {
+        show_admin_bar(false);
+    }
+}
+
 add_action('init', 'register_new_block_type');
 add_action('wp_enqueue_scripts', 'attach_files');
+add_action('wp_loaded', 'no_admin_bar');
